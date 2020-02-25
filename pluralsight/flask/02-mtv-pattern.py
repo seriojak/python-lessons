d02844f961f8d8d61ec90945856d16f23a7a06c2
Serghei Colesnic <serghei.colesnic@outlook.com>
2020-02-25 11:23:45 +0200

    add flask framework

---
+++ pluralsight/flask/02-mtv-pattern.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return render_template("welcome.html",
+                           message="Here's a message from the view.")
